c7137834820227f53224f6cda70c5a8622fe206a
mcc <andi.m.mcclure@gmail.com>
2023-08-10 17:39:13 -0400

    Attenuate entire led grid but it doesn't work

--- src/counter.py
+++ src/counter.py
@@ -8,7 +8,7 @@ class Counter(am.Elaboratable):
         self.count = am.Signal(n)
 
         # Output a single-bit output signal
-        self.has_overflow = need_overflow
+        self._has_overflow = need_overflow
         if need_overflow:
             self.overflow = am.Signal()
 
@@ -17,15 +17,29 @@ class Counter(am.Elaboratable):
         # Count up each clock cycle
         m.d.sync += self.count.eq(self.count + 1)
         # Output overflow on cycles where the count is 0
-        if self.has_overflow:
+        if self._has_overflow:
             m.d.comb += self.overflow.eq(self.count == 0)
         return m
 
 
 class Top(am.Elaboratable):
-    def __init__(self):
+    def __init__(self, attenuate_power, highlight_bits):
+        assert highlight_bits > 0
+
         # State
-        self.current_led = Counter(4)
+        # 0-1: Must be 0 to display;
+        # next bit: highlight
+        # next 2 bits: row
+        # next 2 bits: column
+        # last b
+        self.current_led = Counter(attenuate_power+5)
+        self._led_attenuate = attenuate_power > 0
+        self._led_attenuate_bits = range(0, attenuate_power)
+        self._aled_bits = range(attenuate_power, attenuate_power+2)
+        self._kled_bits = range(attenuate_power+2, attenuate_power+4)
+        self._highlight_bits = range(attenuate_power+4, attenuate_power+4+highlight_bits) # active 0
+
+        self.may_light = am.Signal(1)
 
     def elaborate(self, platform):
         m = am.Module()
@@ -37,6 +51,12 @@ class Top(am.Elaboratable):
         kleds = [platform.request("kled", i) for i in range(4)]
         aled = platform.request("aled", 0)
 
+        # Logic
+        self.may_light.eq(
+            self.current_led.count[self._led_attenuate_bits] == 0
+                if self._led_attenuate else m.C(1)
+        )
+
         # Source: https://github.com/dadamachines/doppler-FPGA-firmware/blob/a3d57bb/doppler_simple_io/doppler_simple_io.v#L153-L168
         # Anode looks like (inverted) column select:
         # * 1110 for LEDs 0, 4,  8, 12
@@ -45,15 +65,18 @@ class Top(am.Elaboratable):
         # * 0111 for LEDs 3, 7, 11, 15
         for i in range(4):
             m.d.comb += \
-                aled.o[0].eq(self.current_led.count % 4 == i)
+                aled.o[0].eq(self.current_led.count[self._aled_bits] == i)
 
         # Cathode looks like row select, but to turn off, we put it in high
         # impedance, rather than grounding it:
 
         m.d.comb += [kled.o.eq(1) for kled in kleds]
         for i in range(4):
+            counter_match = (self.current_led.count[self._kled_bits] == i)
+            if i != 0:
+                counter_match = counter_match & (self.current_led.count[self._highlight_bits]) # Slowest-changing bit(s)
             m.d.comb += \
-                kleds[i].oe.eq(self.current_led.count // 4 == i)
+                kleds[i].oe.eq(counter_match)
 
         # This button isn't debounced, so it'll probably skip a whole lot every time you press it.
 #        button_last = am.Signal()
@@ -65,6 +88,6 @@ class Top(am.Elaboratable):
 
 
 if __name__ == "__main__":
-    top = Top()
+    top = Top(2,1)
     plat = DopplerPlatform()
     plat.build(top)
